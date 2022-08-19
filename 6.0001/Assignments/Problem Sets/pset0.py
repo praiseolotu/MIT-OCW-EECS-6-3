b20e46be0a4b6d40f277148537aa02101c1b4a75
Praise Jah <olioxxayo@gmail.com>
2022-08-19 05:08:21 +0100

    Update pset0.py

--- 6.0001/Assignments/Problem Sets/pset0.py
+++ 6.0001/Assignments/Problem Sets/pset0.py
@@ -1,1 +1,9 @@
-#pending
+import math,os,threading, time, sys
+
+a = int(input("Enter first number: "))
+b = int(input("Enter power: "))
+#get the power value
+c = int(pow(a, b))
+print(a,"**",b, "=",c)
+#print logarithm using math function
+print(math.log(a,a))
